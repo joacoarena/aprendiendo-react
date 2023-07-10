22110b7c0fbdf9af043c256c75a147bd36592663
joacoarena <joaquin.t.arena@gmail.com>
2023-07-09 22:50:28 -0300

    app

---
+++ projects/hello world/src/App.jsx
@@ -0,0 +1,18 @@
+export function App (){
+    return (
+        <article>
+            <header>
+                <img src="" alt="The avatar of someone" />
+                <div>
+                    <strong>roBERTO</strong>
+                    <span>@robertocharles</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
